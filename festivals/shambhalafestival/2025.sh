c1f4f51f66660891ad0b5cf74cb8e8616c769c42
Derek Ellis <me@ellisd.com>
2025-06-16 16:23:02 -0700

    feat: remove modified_at to reduce diff noise

--- festivals/shambhalafestival/2025.sh
+++ festivals/shambhalafestival/2025.sh
@@ -11,6 +11,10 @@ DESTINATION_DIR=$(pwd)/2025
 mkdir -p "${DESTINATION_DIR}"
 
 # Fetching lineup
-curl -H 'content-type: application/json' -H 'accept: application/json' --compressed -H "x-protect: $SESSION_KEY" -H 'accept-language: en' -H 'user-agent: ShambhalaFestival-2025/9526 CFNetwork/3826.500.131 Darwin/24.5.0' -H 'x-app-version: 7.0.0' -H 'x-os-version: 18.5' -H 'x-platform: ios' 'https://app.appmiral.com/api/v7/events/shambhalafestival/editions/shambhalafestival2025/artists?max_per_page=500' | jq > "${DESTINATION_DIR}/shambhalafestival.artists.json"
+curl -H 'content-type: application/json' -H 'accept: application/json' --compressed -H "x-protect: $SESSION_KEY" -H 'accept-language: en' -H 'user-agent: ShambhalaFestival-2025/9526 CFNetwork/3826.500.131 Darwin/24.5.0' -H 'x-app-version: 7.0.0' -H 'x-os-version: 18.5' -H 'x-platform: ios' 'https://app.appmiral.com/api/v7/events/shambhalafestival/editions/shambhalafestival2025/artists?max_per_page=500' | \
+  # Remove the modified_at field from the JSON data
+  jq 'del(..|.modified_at?)' > "${DESTINATION_DIR}/shambhalafestival.artists.json"
 # Fetching stages
-curl -H 'content-type: application/json' -H 'accept: application/json' --compressed -H "x-protect: $SESSION_KEY" -H 'accept-language: en' -H 'user-agent: ShambhalaFestival-2025/9526 CFNetwork/3826.500.131 Darwin/24.5.0' -H 'x-app-version: 7.0.0' -H 'x-os-version: 18.5' -H 'x-platform: ios' 'https://app.appmiral.com/api/v7/events/shambhalafestival/editions/shambhalafestival2025/stages?max_per_page=500' | jq > "${DESTINATION_DIR}/shambhalafestival.stages.json"
+curl -H 'content-type: application/json' -H 'accept: application/json' --compressed -H "x-protect: $SESSION_KEY" -H 'accept-language: en' -H 'user-agent: ShambhalaFestival-2025/9526 CFNetwork/3826.500.131 Darwin/24.5.0' -H 'x-app-version: 7.0.0' -H 'x-os-version: 18.5' -H 'x-platform: ios' 'https://app.appmiral.com/api/v7/events/shambhalafestival/editions/shambhalafestival2025/stages?max_per_page=500' | \
+  # Remove the modified_at field from the JSON data
+  jq 'del(..|.modified_at?)' > "${DESTINATION_DIR}/shambhalafestival.stages.json"
